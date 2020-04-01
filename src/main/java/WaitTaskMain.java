99c05323d5fd9297d735d1b123de466625550cda
qbzhang <javawork@163.com>
2020-04-01 19:24:27 +0800

    update

--- src/main/java/WaitTaskMain.java
+++ src/main/java/WaitTaskMain.java
@@ -4,19 +4,17 @@ import java.util.stream.IntStream;
 public class WaitTaskMain {
     public static void main(String[] args) {
         WaitTask waitTask = new WaitTask();
-        new Thread(() -> {
-            IntStream.range(1, 100).forEach(x -> {
-                if (x != 50)
-                    System.out.println(x);
-                else {
-                    try {
-                        waitTask.await();//暂停线程
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        new Thread(() -> IntStream.range(1, 100).forEach(x -> {
+            if (x != 50)
+                System.out.println(x);
+            else {
+                try {
+                    waitTask.await();//暂停线程
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            });
-        }).start();
+            }
+        })).start();
         try {
             TimeUnit.SECONDS.sleep(10);
         } catch (InterruptedException e) {
